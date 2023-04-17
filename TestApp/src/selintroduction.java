36d7c71d1b923946f9bb486e7daed7c888cbddea
Meenakshi Pal <pal.meenkashi@outlook.com>
2023-04-17 22:14:26 +1000

    sel introduction

---
+++ TestApp/src/selintroduction.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class selintroduction {
+    public static void main(String[] args) {
+       
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.w3schools.com");
+        System.out.println(driver.getTitle()); 
+        driver.close();
+        driver.quit();
+
+
+
+
+
+
+
+    }
+}
